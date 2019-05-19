c7ba040f4b5560a985ff7788aaf4a567a7128758
andres <andresrodriguezdiaz99@gmail.com>
2019-05-18 23:38:54 -0400

    Se agrega lodash y data-table en detalle

--- src/views/Detalle.vue
+++ src/views/Detalle.vue
@@ -1,13 +1,12 @@
-<<template>
-   <div>
+<template>
+  <div>
     <v-container grid-list-md>
       <v-layout row wrap pb-4>
         <v-flex xs12 sm6 pr-5>
           <div class="headline mb-2 font-weight-medium">{{ titulo }}</div>
           <v-layout column>
-            <p>
-              {{ item.descripcion }}
-            </p>
+            <p>{{ item.descripcion }}</p>
+            <h2 class="red--text headline font-weight-medium">${{ item.precio }} C/U</h2>
           </v-layout>
         </v-flex>
         <v-flex xs12 sm6>
@@ -16,6 +15,36 @@
           </v-layout>
         </v-flex>
       </v-layout>
+      <v-layout>
+        <v-expansion-panel v-model="panel" expand>
+          <v-expansion-panel-content>
+            <template v-slot:header>
+              <div class="subheading font-weight-medium text-uppercase">Ficha Técnica</div>
+            </template>
+            <v-card>
+              <v-card-text>
+                <v-data-table
+                  :headers="headers"
+                  :items="getTable"
+                  hide-actions
+                  class="text-capitalize"
+                >
+                  <template slot="headerCell" slot-scope="{ header }">
+                    <span
+                      class="subheading font-weight-light text-success text--darken-3"
+                      v-text="header.text"
+                    />
+                  </template>
+                  <template slot="items" slot-scope="{ item }">
+                    <td>{{ item.atributo }}</td>
+                    <td>{{ item.detalle }}</td>
+                  </template>
+                </v-data-table>
+              </v-card-text>
+            </v-card>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-layout>
     </v-container>
   </div>
 </template>
@@ -23,6 +52,23 @@
 <script>
 import { mapState, mapActions } from "vuex";
 export default {
+  data() {
+    return {
+      panel: [true],
+      headers: [
+        {
+          sortable: false,
+          text: "Atributo",
+          value: "atributo"
+        },
+        {
+          sortable: false,
+          text: "Detalle",
+          value: "detalle"
+        }
+      ]
+    };
+  },
   props: ["id"],
   created() {
     this.$store.dispatch("product/fetchProduct", this.id);
@@ -33,6 +79,17 @@ export default {
     },
     getImagen() {
       return require("@/assets/" + this.item.imagen);
+    },
+    getTable() {
+      const removeIndex = [0, 4, 10, 11, 12, 13, 14];
+      let items = Object.keys(this.item).map(data => ({
+        atributo: data,
+        detalle: this.item[data]
+      }));
+      for (let i = removeIndex.length - 1; i >= 0; i--) {
+        items.splice(removeIndex[i], 1);
+      }
+      return items;
     },
     ...mapState({
       item: state => state.product.product
